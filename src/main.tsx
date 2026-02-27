6ddbcdd6d93f78c1e40b95073e32c31c2da534d0
Eline H. <eline.haukanes@scaleaq.com>
2026-02-27 14:59:01 +0100

    starting app-routes and Apps

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import './index.css'
 import App from './App'
 
 const port = import.meta.env.VITE_PORT
